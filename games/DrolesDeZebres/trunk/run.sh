2de0031e1132cf2e23d7a29e88249590d0ecbbdd
snowgoon88 <snowgoon88@f6258354-9ff4-0310-bb56-f394ca02ebdf>
2007-08-21 17:46:44 +0000

    Example of project configuration

--- games/DrolesDeZebres/trunk/run.sh
+++ games/DrolesDeZebres/trunk/run.sh
@@ -2,7 +2,7 @@
 #
 set LIB_SWT_PATH="/usr/local/eclipse/plugins"
 set LIB_SWT="${LIB_SWT_PATH}/org.eclipse.swt_3.2.0.v3232o.jar:${LIB_SWT_PATH}/org.eclipse.swt.gtk.linux.x86_3.2.0.v3232m.jar"
-set LIB_JARG="/home/dutech/Windaube/Java/lib/jargs.jar"
+set LIB_JARG="/usr/local/Java/lib/jargs.jar"
 
 set CLASS_PATH="${LIB_SWT}:${LIB_JARG}:./bin"
 
